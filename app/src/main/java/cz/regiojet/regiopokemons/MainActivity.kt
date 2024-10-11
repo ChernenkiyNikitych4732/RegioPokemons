4c0d71ae3b49185d781aa364862c169459a89d49
Nikita <n.tchernenky@yandex.ru>
2024-10-11 11:02:28 +0200

    RegioPokemons technický úkol pro společnost STUDENT AGENCY / RegioJet na pozici Android Developer/ka

--- app/src/main/java/cz/regiojet/regiopokemons/MainActivity.kt
+++ app/src/main/java/cz/regiojet/regiopokemons/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -62,7 +63,7 @@ fun MainActivityScreen(modifier: Modifier = Modifier) {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = " Hello Android!",
+        text = "  Hello Android!  ",
         modifier = modifier
     )
 }
